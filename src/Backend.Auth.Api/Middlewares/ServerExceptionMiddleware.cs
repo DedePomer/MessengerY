15e6a096d83a95befc4542e02753e910e9344c27
t343s <t343s@PC-01>
2025-11-11 01:42:27 +0300

    Initial commit

---
+++ src/Backend.Auth.Api/Middlewares/ServerExceptionMiddleware.cs
@@ -0,0 +1,19 @@
+﻿namespace Backend.Auth.Api.Middlewares;
+
+public class ServerExceptionMiddleware(RequestDelegate next, ILogger<ServerExceptionMiddleware> logger)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Auth API server error");
+            
+            context.Response.StatusCode = 500;
+            await context.Response.WriteAsJsonAsync("Server error");
+        }
+    }
+}
