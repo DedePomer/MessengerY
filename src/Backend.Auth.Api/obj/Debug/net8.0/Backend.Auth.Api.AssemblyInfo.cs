3f02b1ab54e8af41e4bb25f056f7f40c27147142
t343s <t343s@PC-01>
2025-11-11 08:22:05 +0300

    feat: миграция

--- src/Backend.Auth.Api/obj/Debug/net8.0/Backend.Auth.Api.AssemblyInfo.cs
+++ src/Backend.Auth.Api/obj/Debug/net8.0/Backend.Auth.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend.Auth.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fef0d449188523f6af4074e89921484b4a738b61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24450f04602e058783c3f965f8cc5ce30f0b57f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend.Auth.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend.Auth.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Backend.Auth.Application/obj/Debug/net8.0/Backend.Auth.Application.AssemblyInfo.cs
+++ src/Backend.Auth.Application/obj/Debug/net8.0/Backend.Auth.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend.Auth.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79922e796fe9e865394a74c23e927f76f14495a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24450f04602e058783c3f965f8cc5ce30f0b57f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend.Auth.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend.Auth.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
