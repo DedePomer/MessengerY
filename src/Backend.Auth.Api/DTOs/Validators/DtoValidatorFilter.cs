66180b1845a531ea9a86aea20c62198634bd8c48
t343s <t343s@PC-01>
2025-11-11 19:48:55 +0300

    feat: добавил фильтр

---
+++ src/Backend.Auth.Api/DTOs/Validators/DtoValidatorFilter.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace Backend.Auth.Api.DTOs.Validators;
+
+public class DtoValidatorFilter<T>(IValidator<T> validator): IEndpointFilter
+{
+    
+    public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+    {
+        var dto = context.Arguments.OfType<T>().FirstOrDefault();
+        if (dto is null)
+            return Results.BadRequest("Invalid request body");
+
+        var result = await validator.ValidateAsync(dto);
+        if (!result.IsValid)
+            return Results.BadRequest(result.Errors);
+
+        return await next(context);
+    }
+}
--- src/Backend.Auth.Common/obj/Debug/net8.0/Backend.Auth.Common.AssemblyInfo.cs
+++ src/Backend.Auth.Common/obj/Debug/net8.0/Backend.Auth.Common.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend.Auth.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1e54489b176630e098c5374d411b787406f5c9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf9f13275b65757777ebc371b09f0ff2c6b567a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend.Auth.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend.Auth.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
