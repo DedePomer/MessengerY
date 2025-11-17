59f1a6a1c569d97fa195ad68db879d1d226151e8
t343s <t343s@PC-01>
2025-11-17 13:12:43 +0300

    feat: добавил типов для получения информации из файла с информацией о путях

---
+++ src/Frontend.Auth.Application/Model/DataType/Route.cs
@@ -0,0 +1,8 @@
+﻿namespace Frontend.Auth.Application.Model.DataType;
+
+internal class Route
+{
+    internal required string Name { get; init; }
+    internal required string PathPrefix { get; init; }
+    internal required string HttpMethod { get; init; }
+}
---
+++ src/Frontend.Auth.Application/Model/DataType/RoutesInformation.cs
@@ -0,0 +1,7 @@
+﻿namespace Frontend.Auth.Application.Model.DataType;
+
+internal class RoutesInformation
+{
+    internal required string GatewayHost { get; init; }
+    internal required List<Route> Routes { get; init; }
+}
